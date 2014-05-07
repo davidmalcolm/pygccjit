81b05c60312776aca77fff4c44b01efc28858f8c
David Malcolm <dmalcolm@redhat.com>
2014-05-07 18:25:53 -0400

    Start adding Sphinx-based documentation

---
+++ doc/trivial-example.rst
@@ -0,0 +1,33 @@
+.. Copyright 2014 David Malcolm <dmalcolm@redhat.com>
+   Copyright 2014 Red Hat, Inc.
+
+   This is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.
+
+Creating a trivial machine code function
+----------------------------------------
+Consider this C function:
+
+ .. code-block:: c
+
+   int square(int i)
+   {
+     return i * i;
+   }
+
+We can implement this using gccjit as follows:
+
+   .. literalinclude:: ../examples/square.py
+    :lines: 27-
+    :language: python
---
+++ examples/square.py
@@ -0,0 +1,85 @@
+#   Copyright 2014 David Malcolm <dmalcolm@redhat.com>
+#   Copyright 2014 Red Hat, Inc.
+#
+#   This is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+"""
+This examples creates and runs the equivalent of this C function:
+
+  int square(int i)
+  {
+    return i * i;
+  }
+"""
+
+import ctypes
+
+import gccjit
+
+int_int_func_type = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+
+def create_fn():
+    # Create a compilation context:
+    ctxt = gccjit.Context()
+
+    # Turn these on to get various kinds of debugging:
+    if 0:
+        ctxt.set_bool_option(gccjit.BoolOption.DUMP_INITIAL_TREE, True)
+        ctxt.set_bool_option(gccjit.BoolOption.DUMP_INITIAL_GIMPLE, True)
+
+    # Adjust this to control optimization level of the generated code:
+    if 0:
+        ctxt.set_int_option(gccjit.IntOption.OPTIMIZATION_LEVEL, 0)
+
+    # Create parameter "i":
+    param_i = ctxt.new_param(ctxt.get_type(gccjit.TypeKind.INT),
+                             b'i')
+    # Create the function:
+    fn = ctxt.new_function(gccjit.FunctionKind.EXPORTED,
+                           ctxt.get_type(gccjit.TypeKind.INT),
+                           b"square",
+                           [param_i])
+
+    # Create a basic block within the function:
+    block = fn.new_block(b'entry')
+
+    # This basic block is relatively simple:
+    block.end_with_return(
+        ctxt.new_binary_op(gccjit.BinaryOp.MULT,
+                           ctxt.get_type(gccjit.TypeKind.INT),
+                           param_i, param_i))
+
+    # Having populated the context, compile it.
+    jit_result = ctxt.compile()
+
+    # This is what you get back from ctxt.compile():
+    assert isinstance(jit_result, gccjit.Result)
+
+    return jit_result
+
+if __name__ == '__main__':
+    jit_result = create_fn()
+
+    # Look up a specific machine code routine within the gccjit.Result,
+    # in this case, the function we created above:
+    void_ptr = jit_result.get_code(b"square")
+
+    # Now use ctypes.CFUNCTYPE to turn it into something we can call
+    # from Python:
+    code = int_int_func_type(void_ptr)
+
+    # Now try running the code:
+    result = code(5)
+    assert result == 25
