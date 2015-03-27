363ebae8f971009a9160219055e5e40212d18a61
David Malcolm <dmalcolm@redhat.com>
2015-03-27 17:32:44 -0400

    0.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ Programming Language :: Python :: 3
 
 setup(
     name='gccjit',
-    version='0.2',
+    version='0.3',
     author="David Malcolm",
     author_email="jit@gcc.gnu.org",
     url="https://github.com/davidmalcolm/pygccjit",
